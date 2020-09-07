6e690fbeed49e13c1c2850c795dae813d6765dfe
Jacopo Lerda <70854171+Jlerda@users.noreply.github.com>
2020-09-07 16:39:01 +0200

    Add option for multiple dices e and sum of the rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,17 @@
 import random
 def main():
+  dice_rolls=2
+  dice_sum=0
   first=1
   last=6
-
-  roll=random.randint(first, last)
-  print(f'You rolled a {roll}')
+  for i in range(0,dice_rolls):
+  
+    roll=random.randint(first, last)
+    dice_sum= dice_sum+roll
+    print(f'You rolled a {roll}')
+  
+  print(f'You have rolled a total of {dice_sum}')
+  
 
 if __name__== "__main__":
   main()
