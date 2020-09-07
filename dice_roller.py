ca11dc3bf0cad1b3a78274c6aad620a6741c5583
Jacopo Lerda <70854171+Jlerda@users.noreply.github.com>
2020-09-07 16:56:24 +0200

    Insert options for user to enter numbers of dice e size

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,21 @@
 import random
 def main():
-  dice_rolls=2
+  dice_rolls=int(input('How many dice would you like to roll?'))
   dice_sum=0
   first=1
-  last=6
+  last=int(input('How many sides are the dice?'))
   for i in range(0,dice_rolls):
   
     roll=random.randint(first, last)
     dice_sum= dice_sum+roll
-    print(f'You rolled a {roll}')
-  
+    if roll==1:
+      print(f'You rolled a {roll}! Epic fail!')
+    
+    elif roll==last:
+      print(f'You rolled a {roll}! Amazing success!')
+
+    else:
+      print(f'You rolled a {roll}')  
   print(f'You have rolled a total of {dice_sum}')
   
 
